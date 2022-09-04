6ee4d0edbf4842563bef9a5f5d1dc11333270cc4
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2022-09-04 23:15:41 +0300

    Completed 3rd and 4th labs

---
+++ scripts/model.js
@@ -0,0 +1,25 @@
+
+class ElectricDecoration {
+  
+    constructor(
+    name, //String
+    material, //String
+    usage, //String
+    amountAvalaible, //Integer
+    colors, //String
+    length, //Float
+    lampsPerMeter, //Integer
+    power, //Integer
+    price //Float
+  ) {
+    this.name = name;
+    this.material = material;
+    this.usage = usage;
+    this.amount_avalaible = amountAvalaible;
+    this.colors = colors;
+    this.length = length;
+    this.lamps_per_meter = lampsPerMeter;
+    this.power = power;
+    this.price = price;
+  }
+}
